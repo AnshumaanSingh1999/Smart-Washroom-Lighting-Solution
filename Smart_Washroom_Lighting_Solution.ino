903332d6a6a59b621a1902d7b1059f7712ddd878
Anshumaan Singh <47323148+AnshumaanSingh1999@users.noreply.github.com>
2021-11-25 01:56:29 +0530

    Add files via upload

---
+++ Smart_Washroom_Lighting_Solution.ino
@@ -0,0 +1,49 @@
+const int PHOTOpin = A0;
+const int PIRpin1 = 2;
+const int PIRpin2 = 3;  
+const int PIRpin3 = 4; 
+const int LEDpin1 = 8;  
+const int LEDpin2 = 9;  
+const int LEDpin3 = 10;
+const int LEDpin4 = 11;  
+void setup() {  
+  Serial.begin(9600);  
+  pinMode(PIRpin1, INPUT); 
+  pinMode(PIRpin2, INPUT);  
+  pinMode(PIRpin3, INPUT);  
+  pinMode(PHOTOpin, INPUT);  
+  pinMode(LEDpin1, OUTPUT);
+  pinMode(LEDpin2, OUTPUT);  
+  pinMode(LEDpin3, OUTPUT);  
+  pinMode(LEDpin4, OUTPUT);  
+}  
+void loop() {  
+  int sensorStatusphoto = analogRead(PHOTOpin); 
+  int sensorStatuspir1 = digitalRead(PIRpin1);
+  int sensorStatuspir2 = digitalRead(PIRpin2);
+  int sensorStatuspir3 = digitalRead(PIRpin3);
+  if (sensorStatusphoto <200){
+    digitalWrite(LEDpin1, HIGH); 
+  }  
+  else{
+    digitalWrite(LEDpin1, LOW); 
+  }
+  if(sensorStatuspir1 == HIGH){
+    digitalWrite(LEDpin3, HIGH);
+  }
+  else{
+    digitalWrite(LEDpin3, LOW); 
+  }
+  if(sensorStatuspir2 == HIGH){
+    digitalWrite(LEDpin2, HIGH);
+  }
+  else{
+    digitalWrite(LEDpin2, LOW); 
+  }
+  if(sensorStatuspir3 == HIGH){
+    digitalWrite(LEDpin4, HIGH);
+  }
+  else{
+    digitalWrite(LEDpin4, LOW); 
+  } 
+}
